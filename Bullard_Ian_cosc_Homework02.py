a1626466223bd9f272fc91cc3082644a627c8643
IanBullard74 <ibullard@uwyo.edu>
2024-10-22 02:44:01 +0000

    added dictionary

---
+++ Bullard_Ian_cosc_Homework02.py
@@ -0,0 +1,20 @@
+# Ian Bullard
+# UWYO COSC 1010
+# Submission Date 10/22/24
+# HW 02
+# Lab Section: 14
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+morse_code_letters ={
+    'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.', 
+    'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---', 
+    'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---', 
+    'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-', 
+    'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--', 
+    'Z': '--..'
+}
+
+
+input()
